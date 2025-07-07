394015d0aab5d50f68944a276084bfc759b0a57c
Vittorio Iocolano <pryderi.mail@gmail.com>
2025-07-07 11:55:49 +0100

    Update motor controller to support steering

--- include/MotorController.h
+++ include/MotorController.h
@@ -2,50 +2,86 @@
 
 #include <Display.h>
 
-const int pwmChannel = 0;
+const int pwmChannel1 = 0;
+const int pwmChannel2 = 1;
+
 const int freq = 50000;
 const int resolution = 8;
-const int maxDuty = (1 << resolution) - 1;
+const int maxDutyRange = (1 << resolution) - 1;
+
+const double minDutyPercent = 0.10;
+const double maxDutyPercent = 0.75;
+
+const uint32_t minDuty = static_cast<uint32_t>(maxDutyRange * minDutyPercent);
+const uint32_t maxDuty = static_cast<uint32_t>(maxDutyRange * maxDutyPercent);
 
 class MotorController {
  public:
   void begin() {
-    digitalWrite(PIN_MOTOR_SLP, LOW);
-    pinMode(PIN_MOTOR_IN1, OUTPUT);
-    pinMode(PIN_MOTOR_IN2, OUTPUT);
-    ledcSetup(pwmChannel, freq, resolution);
-    ledcAttachPin(PIN_MOTOR_IN2, pwmChannel);
-    ledcWrite(pwmChannel, 0);
+    pinMode(PIN_MOTOR_STBY, OUTPUT);
+    pinMode(PIN_MOTOR_A_IN1, OUTPUT);
+    pinMode(PIN_MOTOR_A_IN2, OUTPUT);
+    pinMode(PIN_MOTOR_B_IN1, OUTPUT);
+    pinMode(PIN_MOTOR_B_IN2, OUTPUT);
+
+    digitalWrite(PIN_MOTOR_STBY, LOW);
+
+    ledcSetup(pwmChannel1, freq, resolution);
+    ledcAttachPin(PIN_MOTOR_A_IN2, pwmChannel1);
+    ledcWrite(pwmChannel1, 0);
+
+    ledcSetup(pwmChannel2, freq, resolution);
+    ledcAttachPin(PIN_MOTOR_B_IN2, pwmChannel2);
+    ledcWrite(pwmChannel2, 0);
   }
 
-  void setMotor(int speedRange) {
-    speedRange = constrain(speedRange, -100, 100);
+  void setMotors(long throttle, long balance = 0) {
+    throttle = constrain(throttle, -1000, 1000);
+    balance = constrain(balance, -1000, 1000);
 
-    if (speedRange == 0) {
-      Display::render("SLP", 4);
-      digitalWrite(PIN_MOTOR_SLP, LOW);
-      digitalWrite(PIN_MOTOR_IN1, LOW);
-      digitalWrite(PIN_MOTOR_IN2, LOW);
-      ledcWrite(pwmChannel, 0);
-      Display::render("R - OFF", 5);
+    if (throttle == 0) {
+      Display::render("SLP", 2);
+      digitalWrite(PIN_MOTOR_STBY, LOW);
+      setMotorInternal(0, PIN_MOTOR_A_IN1, PIN_MOTOR_A_IN2, pwmChannel1);
+      setMotorInternal(0, PIN_MOTOR_B_IN1, PIN_MOTOR_B_IN2, pwmChannel2);
+      Display::render("R - OFF", 3);
+      Display::render("L - OFF", 4);
       return;
     }
 
-    bool forward = speedRange > 0;
-    int duty = map(abs(speedRange), 0, 100, 0, maxDuty);
+    digitalWrite(PIN_MOTOR_STBY, HIGH);
 
-    float baseVoltage = 5.0;
-    float aproxVoltage = (baseVoltage * duty) / maxDuty;
+    double motor1Speed = throttle * (1.0 + balance / 100.0);
+    double motor2Speed = throttle * (1.0 - balance / 100.0);
 
-    Display::render("", 4);
-    String motorSummaryR = "R  " + String(speedRange) + "% " + String(duty) + "pwm ~" + String(aproxVoltage) + "V";
-    Display::render(motorSummaryR.c_str(), 5);
+    setMotorInternal(motor1Speed, PIN_MOTOR_A_IN1, PIN_MOTOR_A_IN2, pwmChannel1);
+    setMotorInternal(motor2Speed, PIN_MOTOR_B_IN1, PIN_MOTOR_B_IN2, pwmChannel2);
 
-    digitalWrite(PIN_MOTOR_SLP, HIGH);
-    digitalWrite(PIN_MOTOR_IN1, forward ? LOW : HIGH);
-    ledcWrite(pwmChannel, duty);
+    Display::render("", 2);
+    String motorSummaryR = "R  " + String(motor1Speed / 10) + " %";
+    String motorSummaryL = "L  " + String(motor2Speed / 10) + " %";
+    Display::render(motorSummaryR.c_str(), 3);
+    Display::render(motorSummaryL.c_str(), 4);
   }
 
   void handle() {
+  }
+
+ private:
+  void setMotorInternal(double throttle, int pinIn1, int pinIn2, int pwmChannel) {
+    throttle = constrain(throttle, -1000.0, 1000.0);
+
+    if (throttle == 0) {
+      digitalWrite(pinIn1, LOW);
+      digitalWrite(pinIn2, LOW);
+      ledcWrite(pwmChannel, 0);
+      return;
+    }
+
+    bool forward = throttle > 0;
+    uint32_t duty = map(abs(throttle), 0, 1000, minDuty, maxDuty);
+
+    digitalWrite(pinIn1, forward ? LOW : HIGH);
+    ledcWrite(pwmChannel, duty);
   }
 };
