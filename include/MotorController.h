55323ae2037ce67be56470269d1f7b3373ade19d
Andrea <zhang.andrea@outlook.com>
2025-07-13 14:35:44 +0100

    Factor differential thrust out

--- include/MotorController.h
+++ include/MotorController.h
@@ -2,6 +2,8 @@
 
 #include <Display.h>
 
+#include <tuple>
+
 const uint8_t PWM_CHANNEL_A_1 = 0;
 const uint8_t PWM_CHANNEL_A_2 = 1;
 const uint8_t PWM_CHANNEL_B_1 = 2;
@@ -12,7 +14,7 @@ const int resolution = 8;
 const int maxDutyRange = (1 << resolution) - 1;
 
 const double minDutyPercent = 0.10;
-const double maxDutyPercent = 0.75;
+const double maxDutyPercent = 0.15;
 
 const uint32_t minDuty = static_cast<uint32_t>(maxDutyRange * minDutyPercent);
 const uint32_t maxDuty = static_cast<uint32_t>(maxDutyRange * maxDutyPercent);
@@ -53,31 +55,8 @@ class MotorController {
 
     digitalWrite(PIN_MOTOR_STBY, HIGH);
 
-    // Differential drive truth table:
-    // T: 0     S: 0      = L: 0     R: 0
-    // T: 1000  S: 0      = L: 1000  R: 1000
-    // T: 1000  S: 1000   = L: 1000  R: 0
-    // T: 1000  S: -1000  = L: 0     R: 1000
-    // T: 500   S: 1000   = L: 500   R: 0
-    // T: 1000  S: 500    = L: 1000  R: 500
-    // T: 0     S: 0      = L: 0     R: 0
-
-    int motor1Speed = 0;
-    int motor2Speed = 0;
-
-    if (steering > 0) {
-      // Forward right turn
-      motor1Speed = map(steering, 0, 1000, throttle, 0);
-      motor2Speed = throttle;
-    } else if (steering < 0) {
-      // Forward left turn
-      motor1Speed = throttle;
-      motor2Speed = map(steering, 0, -1000, throttle, 0);
-    } else {
-      // Forward straight
-      motor1Speed = throttle;
-      motor2Speed = throttle;
-    }
+    int motor1Speed, motor2Speed;
+    std::tie(motor1Speed, motor2Speed) = getDifferentialEngineThrottles(throttle, steering);
 
     setMotorInternal(motor1Speed, PWM_CHANNEL_A_1, PWM_CHANNEL_A_2);
     setMotorInternal(motor2Speed, PWM_CHANNEL_B_1, PWM_CHANNEL_B_2);
@@ -118,5 +97,34 @@ class MotorController {
     ledcSetup(channel, freq, resolution);
     ledcAttachPin(pin, channel);
     ledcWrite(channel, 0);
+  }
+
+  std::tuple<int, int> getDifferentialEngineThrottles(int throttle, int steering) {
+    // Differential drive truth table:
+    // T: 0     S: 0      = L: 0     R: 0
+    // T: 1000  S: 0      = L: 1000  R: 1000
+    // T: 1000  S: 1000   = L: 1000  R: 0
+    // T: 1000  S: -1000  = L: 0     R: 1000
+    // T: 500   S: 1000   = L: 500   R: 0
+    // T: 1000  S: 500    = L: 1000  R: 500
+
+    int motor1Speed = 0;
+    int motor2Speed = 0;
+
+    if (steering > 0) {
+      // Forward right turn
+      motor1Speed = map(steering, 0, 1000, throttle, 0);
+      motor2Speed = throttle;
+    } else if (steering < 0) {
+      // Forward left turn
+      motor1Speed = throttle;
+      motor2Speed = map(steering, 0, -1000, throttle, 0);
+    } else {
+      // Forward straight
+      motor1Speed = throttle;
+      motor2Speed = throttle;
+    }
+
+    return {motor1Speed, motor2Speed};
   }
 };
