02c371b7012469867857c5471f28f7263064535b
Vittorio Iocolano <pryderi.mail@gmail.com>
2026-05-08 22:50:19 +0100

    feat: add voltage reading via I2C INA219

--- include/SensorsController.h
+++ include/SensorsController.h
@@ -1,18 +1,30 @@
 #pragma once
+#include <Adafruit_INA219.h>
 #include <DHT.h>
 #include <DHT_U.h>
 
-#define DHTTYPE DHT22  // DHT 11
+#define DHTTYPE DHT22
 
+Adafruit_INA219 ina219;
 DHT_Unified dht(PIN_TEMP_SENSOR, DHTTYPE);
 
 int lastCheckMillis = 0;
-const int intervalMillis = 2000;
+const int intervalMillis = 1000;
+
+const char* $(const char* format, ...) {
+  static char buf[20];
+  va_list args;
+  va_start(args, format);
+  vsnprintf(buf, sizeof(buf), format, args);
+  va_end(args);
+  return buf;
+}
 
 class SensorController {
  public:
   static void begin() {
     dht.begin();
+    ina219.begin();
   }
 
   static void handle() {
@@ -27,27 +39,30 @@ class SensorController {
     sensors_event_t event;
 
     dht.temperature().getEvent(&event);
-    if (isnan(event.temperature)) {
-      Serial.println(F("Error reading temperature!"));
-    } else {
-      Serial.print(F("Temperature: "));
-      Serial.print(event.temperature);
-      Serial.println(F("°C"));
-    }
-
     float temperature = event.temperature;
-
     dht.humidity().getEvent(&event);
-    if (isnan(event.relative_humidity)) {
-      Serial.println(F("Error reading humidity!"));
-    } else {
-      Serial.print(F("Humidity: "));
-      Serial.print(event.relative_humidity);
-      Serial.println(F("%"));
+    float humidity = event.relative_humidity;
+
+    Display::render($("%.2f \xF7"
+                      "C %.2f%%",
+                      temperature,
+                      humidity),
+      1);
+
+    float busVoltage = ina219.getBusVoltage_V();
+    float shuntVoltage = ina219.getShuntVoltage_mV() / 1000;
+    float current = ina219.getCurrent_mA();
+    float trueVoltage = busVoltage + shuntVoltage;
+
+    auto batteryLow_V = 4.80;
+    auto batteryHigh_V = 5.00;
+
+    auto batteryPercentage = (trueVoltage - batteryLow_V) / (float)(batteryHigh_V - batteryLow_V) * 100.0f;
+
+    if (batteryPercentage > 100) {
+      batteryPercentage = 100;
     }
 
-    char buf[16];
-    snprintf(buf, sizeof(buf), "%d \xF7", temperature);
-    Display::render(buf, 1);
+    Display::render($("%.2fV %.2fmA %.0f%%", trueVoltage, current, batteryPercentage), 2);
   }
 };
